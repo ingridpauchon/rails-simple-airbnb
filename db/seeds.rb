76de48392e18e1ef08d3f6d5f939ff45e88ee043
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-09-23 15:41:15 +0200

    simplified Airbnb working, started on styling, some remaining

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Fantistic view over Oslo',
+  address: 'Westye Egebergs gate 3B',
+  description: 'Spacious, amazing view',
+  price_per_night: 300,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Hipster flat in Copenhagen',
+  address: 'Norrebro gade 40',
+  description: 'Hipster details, super retro, lovely!',
+  price_per_night: 230,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Romantic scenery in Paris',
+  address: '30 avenue des champs elysees',
+  description: 'You will fall in love here!',
+  price_per_night: 600,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Enjoy the big city life in New-York',
+  address: '5th Ave',
+  description: 'Very central, near central park, cosy appt',
+  price_per_night: 800,
+  number_of_guests: 4
+)
+
